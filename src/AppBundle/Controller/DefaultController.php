2fdd0f706f6349b7fc1cfc8a8fe1d12e7b31aada
borjamunoz <munoz.borja@gmail.com>
2019-08-19 12:50:07 +0200

    Create login action, validate email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,30 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+    
+    public function loginAction(Request $request) {
+        $helpers = $this->get("app.helpers");
+       
+        $json = $request->get('json', null);
+        
+        if($json != null) {
+            $params = json_decode($json);
+            
+            $email = (isset($params->email) ? $params->email : null);
+            $password = (isset($params->password) ? $params->password : null);
+            
+            $emailConstraint = new Assert\Email();
+            $emailConstraint->message = "email not valid";
+            $validateEmail = $this->get("validator")->validate($email, $emailConstraint);
+            
+            if(count($validateEmail) == 0 && ($password != null)) {
+                echo 'DataSuccess';
+            } else {
+                echo 'DataError';
+            }
+        }
+        die();
     }
 
     public function getUsersAction()
