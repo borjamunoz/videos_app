ab827c487087ab4b8d0620e84839a1614e614c8d
borjamunoz <munoz.borja@gmail.com>
2019-08-19 10:26:07 +0200

    First commit

---
+++ src/BackendBundle/Entity/Video.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+/**
+ * Video
+ */
+class Video
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title = 'NULL';
+
+    /**
+     * @var string
+     */
+    private $description = 'NULL';
+
+    /**
+     * @var string
+     */
+    private $status = 'NULL';
+
+    /**
+     * @var string
+     */
+    private $image = 'NULL';
+
+    /**
+     * @var string
+     */
+    private $videoPath = 'NULL';
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt = 'NULL';
+
+    /**
+     * @var \DateTime
+     */
+    private $udpatedAt = 'NULL';
+
+    /**
+     * @var \BackendBundle\Entity\User
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Video
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Video
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Video
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Video
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set videoPath
+     *
+     * @param string $videoPath
+     *
+     * @return Video
+     */
+    public function setVideoPath($videoPath)
+    {
+        $this->videoPath = $videoPath;
+
+        return $this;
+    }
+
+    /**
+     * Get videoPath
+     *
+     * @return string
+     */
+    public function getVideoPath()
+    {
+        return $this->videoPath;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Video
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set udpatedAt
+     *
+     * @param \DateTime $udpatedAt
+     *
+     * @return Video
+     */
+    public function setUdpatedAt($udpatedAt)
+    {
+        $this->udpatedAt = $udpatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get udpatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUdpatedAt()
+    {
+        return $this->udpatedAt;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \BackendBundle\Entity\User $user
+     *
+     * @return Video
+     */
+    public function setUser(\BackendBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \BackendBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
+
